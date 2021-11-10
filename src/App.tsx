0a0956713dd7d6dd4575110ee239e8b88c3936e4
Jeisson Hernan Malaver Gomez <jeisson.malaver@carroya.com>
2021-11-10 11:40:18 -0500

    :sparkles: Use components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ScoreTs from './components/ScoreTs'
+import ScoreJS from './components/ScoreJs'
+import ScoreJsPrototypes from './components/ScoreJsPrototypes'
+import Score from './components/score/Score'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="match-backgorund">
+
+      <h1>Usando TS</h1>
+      <div>
+        <ScoreTs 
+          local= {{name: "Tot", score: 0, shirt: 'white'}}
+          visit= {{name: "Mun", score: 3, shirt: 'red'}}
+        />  
+      </div>
+      
+      <br></br>
+
+      <h1>JS Puro</h1>
+      <div>
+        <ScoreJS
+          local= {{name: "Lei", score: 0, shirt: 'blue'}}
+          visit= {{name: "Ars", score: 2, shirt: 'yellow'}}
+        />
+      </div>
+
+      <br></br>
+
+      <h1>JS con PropTypes</h1>
+      <div>
+        <ScoreJsPrototypes
+          local= {{name: "Mci", score: 0, shirt: 'white'}}
+          visit= {{name: "Cry", score: 2, shirt: 'blue'}}
+        />
+      </div>
+
+      <br></br>
+
+
+
+      <h1>Con ArchivoLogic</h1>
+      <div>
+        <Score
+          local= {{name: "Bur", score: 3, shirt: 'yellow'}}
+          visit= {{name: "Bre", score: 1, shirt: 'red'}}
+        />
+      </div>
     </div>
   );
 }
